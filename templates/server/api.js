18bbe85e637bbdcc32b5fb1530c98a9d10c5cf1f
Erick Katzenstein <erick.katzenstein@gmail.com>
2017-02-05 10:33:09 -0500

    Update api.js

--- templates/server/api.js
+++ templates/server/api.js
@@ -3,7 +3,7 @@ const products = require('./products');
 const reviews = require('./reviews');
 // import products from './products';
 api
-  .get('/express-test', (req, res) => res.send({express: 'working!',})) //demo route to prove api is working
+  .get('/express-test', (req, res) => res.send({express: 'working!'})) //demo route to prove api is working
   .use('/products', products)
   .use('/reviews', reviews)
 // No routes matched? 404.
