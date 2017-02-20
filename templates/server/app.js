80b18ec94046e0b0a81ab940206c7cd490934e72
Erick Katzenstein <erick.katzenstein@gmail.com>
2017-02-20 11:33:11 -0500

    body-parser-json

--- templates/server/app.js
+++ templates/server/app.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
+//json parser
+app.use(bodyParser.json())
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 // Serve our api
