38a96295e02b46afe7f88cafe686e6b2631fabd9
Erick Katzenstein <erick.katzenstein@gmail.com>
2017-03-05 10:51:48 -0500

    Update index.js

--- templates/db/index.js
+++ templates/db/index.js
@@ -5,12 +5,12 @@ const pkg = require('../package.json')
 
 const name = process.env.DATABASE_NAME || pkg.name;
 
-const url = process.env.DATABASE_URL || `postgres://localhost:5432/`;
+const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${url}${name}`));
 
 // create the database instance
-const db = module.exports = new Sequelize(url+name, {
+const db = module.exports = new Sequelize(url, {
   logging: debug, // export DEBUG=sql in the environment to get SQL queries
   define: {
     underscored: true,       // use snake_case rather than camelCase column names
