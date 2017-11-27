d9bea102b56e049d523a422fc22e2b1ca01afa59
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-11-27 20:19:09 +0800

    Fixed `App.js` directory in the "getting started" page.

--- templates/src/components/App/App.js
+++ templates/src/components/App/App.js
@@ -15,7 +15,7 @@ class App extends Component {
           <h2>Welcome to React - Fullstack!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/components/App/App.js</code> and save to reload.
         </p>
         <Link to='about'><button>Test React Router</button></Link>
         <br />
